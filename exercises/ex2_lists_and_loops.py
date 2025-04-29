3339388facc28b940757942d87826bde0e52b346
Randall Bonilla <randallgbc@gmail.com>
2025-04-29 13:27:10 -0600

    Agregar funciones a ex2

--- exercises/ex2_lists_and_loops.py
+++ exercises/ex2_lists_and_loops.py
@@ -2,20 +2,18 @@
 # 2. Recorre la lista con un loop y calcula el promedio.
 # 3. Imprime el resultado.
 
+def calcular_promedio(nums: list[int]) -> float:
+    return sum(nums) / len(nums) if nums else 0
+
+
 def main():
   numbers = [1, 2, 3, 4, 5]
-  total = 0
-  for number in numbers:
-    total += number
-  average = total / len(numbers)
+  average = calcular_promedio(numbers)
   print(f"El promedio de la lista es: {average}")
 
   #Otra forma de hacerlo con input y map
   numbers = list(map(int, input("Ingrese una lista de números separados por espacios: ").split()))
-  total = 0
-  for number in numbers:
-    total += number
-  average = total / len(numbers)
+  average = calcular_promedio(numbers)
   print(f"El promedio de la lista es: {average}")
 
 
