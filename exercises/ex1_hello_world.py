70a8ac79de8119f6c9362fc912741b0d7b92fdc0
Randall Bonilla <randallgbc@gmail.com>
2025-04-29 13:07:44 -0600

    Saludos con nombre format y f-string

--- exercises/ex1_hello_world.py
+++ exercises/ex1_hello_world.py
@@ -2,7 +2,12 @@
 # 2. Pide al usuario su nombre y salúdalo: "¡Hola, <nombre>!"
 
 def main():
-  print("Hello, World!")
+  name = input("¿Cuál es su nombre? ")
+  print("Hello, World!" + name)
+  #Haciendo uso de format
+  print("Hello, World! {}".format(name))
+  #Haciendo uso de f-string
+  print(f"Hello, World! {name}")
 
 if __name__ == "__main__":
   main()
